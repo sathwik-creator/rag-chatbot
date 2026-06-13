d52850dad5fb6488e27d0ea76cbe37a3c05712eb
Dubakunta Sathwik <sathwik.d0007@gmail.com>
2026-06-13 11:05:32 +0530

    Fix dependency conflict

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,7 +1,8 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 def get_embedding_model():
+
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
