695c0e84ce5d54619158dd60f5414b1fafaf2697
Dubakunta Sathwik <sathwik.d0007@gmail.com>
2026-06-19 23:22:58 +0530

    Fix embeddings dependencies

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,10 +1,16 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def get_embedding_model():
 
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={
+            "device": "cpu"
+        },
+        encode_kwargs={
+            "normalize_embeddings": False
+        }
     )
 
     return embeddings
